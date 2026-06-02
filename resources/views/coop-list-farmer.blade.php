5320571f0237c1bd3dfff51d68536b4d156e20b5
John Carlo Villamar <kaizer2407@gmail.com>
2026-06-02 16:03:28 +0800

    Updated dashboard, farmer, cooperative, carabao UI and routes

--- resources/views/coop-list-farmer.blade.php
+++ resources/views/coop-list-farmer.blade.php
@@ -5,6 +5,7 @@
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+<link rel="icon" href="{{ asset('images/pcclogo.jpg') }}" type="image/png">
 
 <style>
 :root{--navy:#083d6f;--navy2:#0d4f8b;--bg:#f4f7fb;}
